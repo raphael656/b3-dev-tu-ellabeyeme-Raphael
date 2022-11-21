d0bdf6c5e432e927b4eef1b38f3b1d0dff95a6d5
raphael656 <74969464+raphael656@users.noreply.github.com>
2022-11-21 14:31:18 +0100

    Ajout des test dans app.test.js pour la class Triangle

--- chai-example/test/app.test.js
+++ chai-example/test/app.test.js
@@ -1,4 +1,5 @@
 const Cube = require('../src/app').Cube;
+const Triangle = require('../src/app').Triangle;
 const expect = require('chai').expect;
 
 describe('Testing the Cube Functions', function() {
@@ -17,6 +18,29 @@ describe('Testing the Cube Functions', function() {
     it('3. The volume of the Cube', function(done) {
         let c3 = new Cube(7);
         expect(c3.getVolume()).to.equal(343);
+        done();
+    });
+    
+});
+
+
+
+describe('Testing the Triangle Functions', function() {
+    it('1. The side length of the Triangle', function(done) {
+        let variables = new Triangle(3,3,3);
+        expect(variables.getSideLength()).deep.to.equal([3,3,3]);
+        done();
+    });
+    
+    it('2. The surface area of the Triangle', function(done) {
+        let Aire = new Triangle(4,4,4,4);
+        expect(Aire.getSurfaceArea()).to.equal(8);
+        done();
+    });
+    
+    it('3. The volume of the Triange', function(done) {
+        let c3 = new Cube(4,4,4,4,4);
+        expect(c3.getVolume()).to.equal(64);
         done();
     });
     
